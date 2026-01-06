efff94685bab2f01a530068f1333aeec2a4d8f86
淵之上航平 <k.fuchinoue@migi-nanameue.co.jp>
2026-01-06 11:48:57 +0900

    fix: 選考詳細ページの左右分割レイアウトを改善
    - h-full min-h-[calc(100vh-64px)]で高さを調整
    - 左側をflex-1 max-w-[calc(100%-400px)]で適切に制約
    - DashboardLayoutとの互換性を改善

--- resume-generator/src/app/selections/[id]/page.tsx
+++ resume-generator/src/app/selections/[id]/page.tsx
@@ -618,9 +618,9 @@ export default function SelectionDetailPage() {
 
   return (
     <DashboardLayout>
-      <div className="flex h-[calc(100vh-64px)] overflow-hidden">
+      <div className="flex h-full min-h-[calc(100vh-64px)]">
         {/* 左側：選考情報（スクロール可能）*/}
-        <div className="w-[calc(100%-400px)] overflow-y-auto p-6">
+        <div className="flex-1 min-w-0 max-w-[calc(100%-400px)] overflow-y-auto p-6">
           {/* CIRCUS風ヘッダー */}
           <div className="mb-6">
             {/* ナビゲーション */}
